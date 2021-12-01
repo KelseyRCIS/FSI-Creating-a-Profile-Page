bb5a6d1fcb972fd306d8f4a77836ac34b7893673
Kelsey Bassanini <KelseyRCIS@gmail.com>
2021-11-30 20:39:35 -0800

    Created a full HTML page using only JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
+
+let image = document.createElement('img')
+image.setAttribute('class', 'dog-image')
+image.setAttribute('src', './assets/rizzo.jpg')
+content.append(image)
